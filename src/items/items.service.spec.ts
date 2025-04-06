7af4c25d2086d7cb392340835698b31dffd7e954
hiroka <mitsunagahrk87@gmail.com>
2025-04-06 13:50:21 +0900

    findAllの単体テスト(正常系)

---
+++ src/items/items.service.spec.ts
@@ -0,0 +1,35 @@
+import { Test } from '@nestjs/testing';
+import { ItemsService } from './items.service';
+import { PrismaService } from '../prisma/prisma.service';
+
+const mockPrismaService = {
+  item: {
+    findMany: jest.fn(),
+  },
+};
+
+describe('ItemsService', () => {
+  let itemsService;
+  let prismaService;
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        ItemsService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+      ],
+    }).compile();
+    itemsService = module.get<ItemsService>(ItemsService);
+    prismaService = module.get<PrismaService>(PrismaService);
+  });
+  describe('findAll', () => {
+    it('正常系', async () => {
+      prismaService.item.findMany.mockResolvedValue([]);
+      const expected = [];
+      const result = await itemsService.findAll();
+      expect(result).toEqual(expected);
+    });
+  });
+});
