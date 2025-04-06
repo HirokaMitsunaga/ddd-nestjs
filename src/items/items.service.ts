6f6babc5e488555da94f7901190b64d3cd4799bc
hiroka <mitsunagahrk87@gmail.com>
2025-04-06 12:21:49 +0900

    jwt認証の実装

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -21,7 +21,7 @@ export class ItemsService {
     return found;
   }
 
-  async create(createItemDto: CreateItemDto): Promise<Item> {
+  async create(createItemDto: CreateItemDto, userId: string): Promise<Item> {
     const { name, price, description } = createItemDto;
 
     return await this.prismaService.item.create({
@@ -30,7 +30,7 @@ export class ItemsService {
         price,
         description,
         status: ItemStatus.ON_SALE,
-        userId: '',
+        userId,
       },
     });
   }
